731ae197354509131b94e087e03fb7e96b064d73
Mi Seon Kang <miseonk@kr.ibm.com>
2018-10-15 20:01:14 +0900

    메일 네이버로 수정

--- src/main/java/com/ibmMeeting/Service/CommonService.java
+++ src/main/java/com/ibmMeeting/Service/CommonService.java
@@ -78,7 +78,7 @@ public class CommonService {
 		
 		  MimeMessage message = emailSender.createMimeMessage();
 		  
-		  message.setFrom("kangms9295@gmail.com");  
+		  message.setFrom("ibm_meeting@naver.com");  
 		  message.addRecipient(RecipientType.TO, new InternetAddress(email));
 		  message.setSubject(subject);
 		  message.setText(content, "utf-8", "html");
